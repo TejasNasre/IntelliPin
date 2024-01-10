47f90ca0a55109a04c657d413511fa57eb931e32
tejasnasre <tejasnasre120@gmail.com>
2024-01-10 10:56:24 +0530

    Setup Passport In User Js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,43 @@
-var express = require('express');
-var router = express.Router();
+const mongoose = require("mongoose");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const plm = require("passport-local-mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/IntellPin");
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  contact: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  profileImage: {
+    type: String,
+    required: true,
+  },
+  boards : {
+    type : Array,
+    default : []
+  }
 });
 
-module.exports = router;
+userSchema.plugin(plm);
+
+module.exports = mongoose.model("User", userSchema);
